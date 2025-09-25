41506286d0d1ea6f67b393f9e876a8f9301c8b4a
IHYDIAF <joshellangelo@gmail.com>
2025-09-24 22:06:16 -0400

    Initial Commit: Bad version of Calculator

---
+++ Bad-Calc.py
@@ -0,0 +1,15 @@
+#Calculator file that presents poor coding practices.
+
+
+def subtract(n1, n2):
+
+    # if n1 > n2:
+    #   print("First number is bigger")
+    # else:
+    #   print("Second number is bigger")
+
+    a = n1
+    b = n2
+
+    return a + b
+
