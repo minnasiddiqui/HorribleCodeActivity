32fc994651a7141c5a607bedb6230e67c9249f8e
IHYDIAF <joshellangelo@gmail.com>
2025-09-24 22:17:00 -0400

    Feat: Finished the Bad Calculator

--- Bad-Calc.py
+++ Bad-Calc.py
@@ -1,5 +1,6 @@
 #Calculator file that presents poor coding practices.
 
+import math
 
 def subtract(n1, n2):
 
@@ -13,3 +14,40 @@ def subtract(n1, n2):
 
     return a + b
 
+class UserProfile():
+    def __init(self, name):
+        self.username = name
+        print(f"Profile for '{self.username}' created.")
+
+def do_calc (p, op, x1, x2):
+    if op =='+':
+        return x1 + x2
+    elif op == '-':
+        return x1 - x2
+    else:
+        return 0
+
+def run_calculator():
+    profile = UserProfile()
+
+    op_choice = input("Enter operation (+ or -): ")
+
+    if op_choice == '+':
+        print ("Please give two numbers for addition")
+        val1 = float(input("Enter first value: "))
+        val2 = float(input("Enter second value: "))
+
+        result = do_calc(profile, '+', val1, val2)
+
+        print(f"The result is ==> {result}")
+
+    elif op_choice == '-':
+        print("Please give two numbers for subtraction.")
+        val1 = float(input("Enter first value: "))
+        val2 = float(input("Enter second value: "))
+
+        result = do_calc(profile, '-', val1, val2)
+
+        print(f"The result is ==> {result}")
+
+run_calculator()
